1a924db1da178d75e6914df16f70b93ad6669987
beata <beata@emc2wave.com>
2020-07-08 16:54:42 -0700

    create ball props

--- script.js
+++ script.js
@@ -4,6 +4,16 @@ const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = document.getContext('2d');
 
+// Create ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => {
   rules.classList.add('show');
