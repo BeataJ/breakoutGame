19bf781ebb58bb157121f7694b3b7569c6d8a293
beata <beata@emc2wave.com>
2020-07-08 17:44:52 -0700

    create drawBall()

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
-const ctx = document.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 // Create ball props
 const ball = {
@@ -13,6 +13,17 @@ const ball = {
   dx: 4,
   dy: -4,
 };
+
+// Draw ball on canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+drawBall();
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => {
